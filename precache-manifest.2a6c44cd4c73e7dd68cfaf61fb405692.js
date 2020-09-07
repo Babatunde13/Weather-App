16e65271ce335d690dae6191690b86445bda22f1
Babatunde13 <koikibabatunde14@gmail.com>
2020-09-07 22:43:13 +0100

    Updates

---
+++ precache-manifest.2a6c44cd4c73e7dd68cfaf61fb405692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483a8d0f6b160240b6179e1a61efe26c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dff1ab7e1eb184a19443",
+    "url": "/static/css/main.5ca35669.chunk.css"
+  },
+  {
+    "revision": "cc29fc16b0709c552df9",
+    "url": "/static/js/2.6b24e23b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b24e23b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff1ab7e1eb184a19443",
+    "url": "/static/js/main.0dce1bf2.chunk.js"
+  },
+  {
+    "revision": "655540d0134b581f0265",
+    "url": "/static/js/runtime-main.da27712b.js"
+  }
+]);
