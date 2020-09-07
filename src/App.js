ebd4a1a5c585bc6d3c869b57f71914451299ec96
Babatunde13 <koikibabatunde14@gmail.com>
2020-09-07 16:43:44 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ class App extends  React.Component {
     return props.name
   }
   render() {
-    console.log(this.state)
     return (
       <div className='container weather'>
         <input 
